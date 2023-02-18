ef62cf2c22c79e8ecf0e0e7c535a2b715e813397
Sun Kim <sunkim924@gmail.com>
2023-02-18 20:31:45 +0000

    add prefect zoomcamp dir

---
+++ week_2_workflow_orchestration/prefect-zoomcamp/flows/03_deployments/docker_deploy.py
@@ -0,0 +1,15 @@
+from prefect.deployments import Deployment
+from prefect.infrastructure.docker import DockerContainer
+from parameterized_flow import etl_parent_flow
+
+docker_block = DockerContainer.load("zoomcamp")
+
+docker_deployment = Deployment.build_from_flow(
+    flow=etl_parent_flow,
+    name="docker-flow",
+    infrastructure=docker_block
+)
+
+
+if __name__ == "__main__":
+    docker_deployment.apply()
